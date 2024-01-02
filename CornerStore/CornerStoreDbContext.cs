d7286acad892426fd03b170b48c7b3501905c8cf
Ryan Ramsey <rramsey1115@gmail.com>
2024-01-02 10:33:18 -0600

    models created

--- CornerStore/CornerStoreDbContext.cs
+++ CornerStore/CornerStoreDbContext.cs
@@ -2,6 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using CornerStore.Models;
 public class CornerStoreDbContext : DbContext
 {
+    public DbSet<Cashier> Cashiers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderProduct> OrderProducts { get; set; }
+    public DbSet<Product> Products { get; set; }
 
     public CornerStoreDbContext(DbContextOptions<CornerStoreDbContext> context) : base(context)
     {
@@ -11,5 +16,6 @@ public class CornerStoreDbContext : DbContext
     //allows us to configure the schema when migrating as well as seed data
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        
     }
 }
--- CornerStore/Models/Order.cs
+++ CornerStore/Models/Order.cs
@@ -1,7 +1,21 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace CornerStore.Models;
 
 public class Order
 {
-
+    public int Id { get; set; }
+    [Required]
+    public int CashierId { get; set; }
+    public Cashier Cashier { get; set; }
+    public List<OrderProduct> OrderProducts { get; set; }
+    public decimal Total {
+        get
+        {
+            decimal price = 0;
+            var res = OrderProducts.Select(p => price += p.Product.Price * p.Quantity );
+            return res != null && res.Any() ? res.Sum() : (decimal)0;
+        }
+    }
 }
